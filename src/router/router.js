4fd4fe1b4e6e133ec9a8cabdb6313b4e13269e5c
vincentyun <vincentzyc@qq.com>
2017-12-13 18:51:40 +0800

    优化路由动画

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Bundle from "./Bundle";
-import { AnimatedSwitch } from "react-router-transition";
+import { spring, AnimatedSwitch } from "react-router-transition";
 import Containers from "../components/Containers";
 
 const Login = props => <Bundle load={() => import("../components/Login")}>{Login => <Login {...props} />}</Bundle>;
@@ -12,6 +12,41 @@ const Me = props => <Bundle load={() => import("../pages/Me")}>{Me => <Me {...pr
 const CheckPhone = props => <Bundle load={() => import("../pages/Check_phone")}>{CheckPhone => <CheckPhone {...props} />}</Bundle>;
 const UserInfo = props => <Bundle load={() => import("../pages/UserInfo")}>{UserInfo => <UserInfo {...props} />}</Bundle>;
 
+// we need to map the `scale` prop we define below
+// to the transform style property
+function mapStyles(styles) {
+    return {
+        opacity: styles.opacity,
+        transform: `scale(${styles.scale})`
+    };
+}
+
+// wrap the `spring` helper to use a bouncy config
+function bounce(val) {
+    return spring(val, {
+        stiffness: 666,  // 20-3000 越大动画时间较短
+        damping: 66   //22
+    });
+}
+
+// child matches will...
+const bounceTransition = {
+    // start in a transparent, upscaled state
+    atEnter: {
+        opacity: 0,
+        scale: 1.2
+    },
+    // leave in a transparent, downscaled state
+    atLeave: {
+        opacity: bounce(0),
+        scale: bounce(0.7)
+    },
+    // and rest at an opaque, normally-scaled state
+    atActive: {
+        opacity: bounce(1),
+        scale: bounce(1)
+    }
+};
 class routerList extends React.Component {
     render() {
         return (
@@ -22,7 +57,13 @@ class routerList extends React.Component {
                         path="/"
                         render={({ history, location }) => (
                             <Containers history={history} location={location}>
-                                <AnimatedSwitch atEnter={{ opacity: 0 }} atLeave={{ opacity: 0 }} atActive={{ opacity: 1 }} className="switch-wrapper">
+                                <AnimatedSwitch
+                                    atEnter={bounceTransition.atEnter}
+                                    atLeave={bounceTransition.atLeave}
+                                    atActive={bounceTransition.atActive}
+                                    mapStyles={mapStyles}
+                                    className="switch-wrapper"
+                                >
                                     <Route exact path="/" component={Home} />
                                     <Route path="/roster" component={Roster} />
                                     <Route path="/todolist" component={Todolist} />
